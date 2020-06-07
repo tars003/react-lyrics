609872614392f9f1906cb53a8e177a6bae9057e8
tars <ajaysharma2ku@gmail.com>
2020-06-07 13:42:48 +0530

    Added build

---
+++ client/build/precache-manifest.5f1f6223daf05c76f80a15ad3256b9b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7996d473ecaf1f30fef0eff3dc954d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdff215c619e661e4a91",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ac6ba3e676f69491a916",
+    "url": "/static/js/2.0462ecda.chunk.js"
+  },
+  {
+    "revision": "fdff215c619e661e4a91",
+    "url": "/static/js/main.58f3b117.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
