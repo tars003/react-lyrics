1dc5ce9d669bd85e480f3c4ccc9ef42b44caa1e0
tars <ajaysharma2ku@gmail.com>
2020-06-07 13:46:38 +0530

    Track component fix

--- client/src/components/tracks/Track.js
+++ client/src/components/tracks/Track.js
@@ -8,12 +8,12 @@ const Track = props => {
     <div className="col-md-6">
       <div className="card mb-4 shadow-sm">
         <div className="card-body">
-          <h5>{track.artist_name}</h5>
+          <h5>{track.track_name}</h5>
           <p className="card-text">
             <strong>
               <i className="fas fa-play" /> Track
             </strong>
-            : {track.track_name}
+            : {track.artist_name}
             <br />
             <strong>
               <i className="fas fa-compact-disc" /> Album
